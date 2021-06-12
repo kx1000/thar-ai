f719514804385b374162e1bdb97334ca6f134961
Kacper <kacper.rogula@gmail.com>
2021-06-12 23:19:10 +0200

    Fixed menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container w-full flex flex-wrap mx-auto px-2 pt-8 lg:pt-16 mt-16">
     <div class="w-full lg:w-1/5 lg:px-6 text-xl text-gray-800 leading-normal">
-      <Header />
+      <Header class="lg:fixed" />
     </div>
-    <div class="w-full lg:w-4/5 p-8 mt-6 lg:mt-0 text-gray-900 leading-normal bg-white border border-gray-400 border-rounded">
+    <div class="w-full lg:w-4/5 mt-6 lg:mt-0 text-gray-900 leading-normal bg-white border border-gray-400 border-rounded">
       <nuxt />
     </div>
   </div>
