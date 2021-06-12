8acfb1c9f9c04e87674b4337300e4d1fa1c5154b
Kacper <kacper.rogula@gmail.com>
2021-06-12 22:30:08 +0200

    Menu

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -1,14 +1,20 @@
 <template>
-  <nav class="scrim-bg fixed z-40 top-0 inset-x-0 pt-3 px-3" aria-label="Main Menu">
-    <ul class="flex">
-      <li class="flex-1">
-        <nuxt-link class="btn block" to="/">Home</nuxt-link>
+  <nav aria-label="Main Menu">
+    <ul class="">
+      <li>
+        <nuxt-link class="btn block" to="/">Thar Ai</nuxt-link>
       </li>
-      <li class="flex-1 ml-2">
-        <nuxt-link class="btn block" to="/blog">Blog</nuxt-link>
+      <li>
+        <nuxt-link class="btn block" to="/biography">Biography</nuxt-link>
       </li>
-      <li class="flex-1 ml-2">
-        <nuxt-link class="btn block" to="/projects">Projects</nuxt-link>
+      <li>
+        <nuxt-link class="btn block" to="/projects">Discography</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link class="btn block" to="/blog">Lyrics</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link class="btn block" to="/contact">Contact</nuxt-link>
       </li>
     </ul>
   </nav>
---
+++ pages/contact/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <section class="w-full max-w-5xl mx-auto">
+      <h1 class="title">Contact</h1>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, error }) {
+    let posts;
+    try {
+      posts = await $content("projects").fetch();
+    } catch (e) {
+      error({ message: "Projects not found" });
+    }
+    return { posts };
+  },
+}
+</script>
