6bbc5f4a23b544ed0e01246a4bf38d8b50f5d75d
Kacper <kacper.rogula@gmail.com>
2021-06-13 09:47:03 +0200

    Background and logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="container w-full flex flex-wrap mx-auto px-2 pt-8 lg:pt-16 mt-16">
+  <div class="container w-full flex flex-wrap mx-auto mx-0 pt-8 lg:pt-16 mt-16">
     <div class="w-full lg:w-1/5 lg:px-6 text-xl text-gray-800 leading-normal">
       <nuxt-link
           class="text-red-600"
           to="/"
       >
-        <span class="pb-1 md:pb-0 text-sm">Thar Ai</span>
+        <span class="pb-1 md:pb-0 text-sm logo">Thar Ai</span>
       </nuxt-link>
       <Header class="lg:fixed" />
       <Links class="hidden lg:block lg:fixed bottom-0" />
@@ -15,3 +15,12 @@
     </div>
   </div>
 </template>
+
+<style>
+  .logo {
+    text-transform: uppercase;
+    font-size: 32px;
+    letter-spacing: 12px;
+    font-family: 'TharAi';
+  }
+</style>
