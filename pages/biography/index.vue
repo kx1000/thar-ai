ecda069d172383225a54abd79c59077d0ca88dac
Kacper <kacper.rogula@gmail.com>
2021-06-13 14:37:29 +0200

    Biography

--- pages/biography/index.vue
+++ pages/biography/index.vue
@@ -2,6 +2,23 @@
   <main>
     <section class="w-full max-w-5xl mx-auto">
       <h1 class="title">Biography</h1>
+      <div>
+        {{ biography }}
+      </div>
     </section>
   </main>
 </template>
+
+<script>
+export default {
+  async asyncData({ $content, error }) {
+    let biography;
+    try {
+      biography = (await $content("site").fetch())[0].biography;
+    } catch (e) {
+      error({ message: "Biography not found" });
+    }
+    return { biography };
+  },
+}
+</script>
