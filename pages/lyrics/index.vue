218549fc1c939aebe2941f9154145bfb58966e76
Kacper <kacper.rogula@gmail.com>
2021-06-13 16:33:33 +0200

    Lyrics by albums

--- pages/lyrics/index.vue
+++ pages/lyrics/index.vue
@@ -1,8 +1,18 @@
 <template>
   <main>
-    <section v-if="posts" class="w-full max-w-5xl mx-auto">
+    <section v-if="albums" class="w-full max-w-5xl mx-auto">
       <h1 class="title">Lyrics</h1>
-      <posts post-type="lyrics" :amount="100"/>
+      <div v-for="album in albums">
+        <h1>album: {{ album.title }}</h1>
+        <p v-for="lyric in album.lirycs">
+          <nuxt-link
+              :to="`lyrics/${lyric.slug}`"
+              class="card card--clickable"
+          >
+           {{ lyric.number }} {{ lyric.title }}
+          </nuxt-link>
+        </p>
+      </div>
     </section>
   </main>
 </template>
@@ -11,12 +21,17 @@
 export default {
   async asyncData({ $content, error }) {
     let posts;
+    let albums;
     try {
-      posts = await $content("lyrics").fetch();
+      albums = await $content('discography').fetch();
+
+      await albums.forEach(async album => {
+        album.lirycs = await $content("lyrics").where({ album: album.slug }).sortBy('number').fetch();
+      });
     } catch (e) {
       error({ message: "Lyrics not found" });
     }
-    return { posts };
+    return { albums };
   },
 }
 </script>
