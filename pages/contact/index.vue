abc4a6022beac0b90f10b3dd3a083c114796eb14
Kacper <kacper.rogula@gmail.com>
2021-06-26 11:49:02 +0200

    Testing contact form

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -2,6 +2,20 @@
   <main>
     <section class="w-full max-w-5xl mx-auto">
       <h1 class="title">Contact</h1>
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </section>
   </main>
 </template>
