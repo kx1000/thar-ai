57dde050986158bbff9af878f7c26b664f964f83
Kacper <kacper.rogula@gmail.com>
2021-06-26 13:12:20 +0200

    Captcha

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -2,7 +2,7 @@
   <main>
     <section class="w-full max-w-5xl mx-auto">
       <h1 class="title">Contact</h1>
-      <form v-if="!isDoneScreen" id="contact" name="contact" v-on:submit.prevent="handleSubmit" data-netlify="true">
+      <form name="contact" netlify-honeypot="bot-field" data-netlify="true">
         <p>
           <label>Your Name<br>
             <input type="text" name="name" />
@@ -24,33 +24,6 @@
           <button type="submit">Send</button>
         </p>
       </form>
-      <div v-else>
-        Email has sent.
-      </div>
     </section>
   </main>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      isDoneScreen: false,
-    }
-  },
-  methods: {
-    handleSubmit() {
-      let myForm = document.getElementById('contact');
-      let formData = new FormData(myForm)
-      fetch('/', {
-        method: 'POST',
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(formData).toString()
-      }).then(() => {
-        this.isDoneScreen = true;
-      }).catch((error) =>
-          alert(error))
-    }
-  }
-}
-</script>
