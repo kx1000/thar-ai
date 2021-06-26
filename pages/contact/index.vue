e28be02b9c57e85a1393d36b38a7944c884719cd
Kacper <kacper.rogula@gmail.com>
2021-06-26 12:57:25 +0200

    Colors

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -2,20 +2,55 @@
   <main>
     <section class="w-full max-w-5xl mx-auto">
       <h1 class="title">Contact</h1>
-      <form name="contact" method="POST" data-netlify="true">
+      <form v-if="!isDoneScreen" id="contact" name="contact" v-on:submit.prevent="handleSubmit" data-netlify="true">
         <p>
-          <label>Your Name: <input type="text" name="name" /></label>
+          <label>Your Name<br>
+            <input type="text" name="name" />
+          </label>
         </p>
         <p>
-          <label>Your Email: <input type="email" name="email" /></label>
+          <label>
+            Your Email<br>
+            <input type="email" name="email" />
+          </label>
         </p>
         <p>
-          <label>Message: <textarea name="message"></textarea></label>
+          <label>
+            Message<br>
+            <textarea name="message"></textarea>
+          </label>
         </p>
         <p>
           <button type="submit">Send</button>
         </p>
       </form>
+      <div v-else>
+        Email has sent.
+      </div>
     </section>
   </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isDoneScreen: false,
+    }
+  },
+  methods: {
+    handleSubmit() {
+      let myForm = document.getElementById('contact');
+      let formData = new FormData(myForm)
+      fetch('/', {
+        method: 'POST',
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString()
+      }).then(() => {
+        this.isDoneScreen = true;
+      }).catch((error) =>
+          alert(error))
+    }
+  }
+}
+</script>
