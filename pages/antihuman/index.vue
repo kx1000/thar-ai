5eb7563fb00641d9b23422a7d2c93c089e2bb1f2
Kacper <kacper.rogula@gmail.com>
2021-07-14 19:13:28 +0200

    Redirect to lyrics

---
+++ pages/antihuman/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+  </main>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'REDIRECTING... ∙ THAR AI',
+  },
+  created() {
+    this.$router.push('/lyrics');
+  }
+}
+</script>
